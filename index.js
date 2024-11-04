e8a2d4b6c4e4b16c173b10b7e190adaf1942508c
Jonatan <Jonatan.goldbach@Telefonica.com>
2024-11-04 10:56:34 +0100

    WIP

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 let btn = document.getElementById("btn");
 let gameContainer = document.getElementById("gameContainer");
+console.log("gameContainer", gameContainer.children);
+let snake = [3, 4, 5];
+let snakeObj = {
+  row: [1],
+  col: [0],
+};
+const rows = 12;
+const cols = 10;
+
+function fillArr(num) {
+  for (let i = 0; i < num; i++) {
+    let arr = [];
+    arr.push(i);
+    return arr;
+  }
+}
+
+const array2D = new Array(rows)
+  .fill()
+  .map((row, index) => new Array(cols).fill(index));
+
+array2D.forEach((row, y) => {
+  row.forEach((cell, x) => {
+    let item = document.createElement("div");
+    item.textContent = cell;
+    item.classList.add("gridItem");
+    item.id = `x${x}y${y}`;
+    gameContainer.appendChild(item);
+  });
+});
 let snakeAlive = true;
-for (let i = 0; i < 120; i++) {
-  let element = document.createElement("div");
-  element.textContent = i;
-  element.classList.add("gridItem");
-  gameContainer.appendChild(element);
-}
-
-function colorSnake(i) {
-  gameContainer.children[i].classList.add("snake");
-}
-
-function removeSnake(i) {
-  gameContainer.children[i].classList.remove("snake");
-}
-
 const timeout = (ms) => new Promise((res) => setTimeout(res, ms));
 (async () => {
-  let i = 0;
   while (snakeAlive == true) {
-    await timeout(1000);
     // iteration logic
-    colorSnake(i);
-    if (i >= 4) {
-      removeSnake(i - 4);
-    }
-    i++;
-    if (i > 9) {
-      i = 0;
-    }
+    await timeout(1000);
+    resetField();
+    moveSnake("horizontally");
+    drawSnake();
     // termination logic
     if (snakeAlive == false) {
       break;
@@ -37,3 +46,92 @@ const timeout = (ms) => new Promise((res) => setTimeout(res, ms));
   }
   console.log("done");
 })();
+
+function moveSnakeObjectHorizontally() {
+  if (snakeObj.row[snakeObj.row.length - 1] < 9) {
+    snakeObj.row.push(snakeObj.row[snakeObj.row.length - 1] + 1);
+  }
+  let shiftedElement = snakeObj.row.shift();
+  if (shiftedElement == 7) {
+    snakeObj.row.push(0);
+  }
+}
+
+function moveSnakeObjectVertically() {
+  if (snakeObj.col[snakeObj.col.length - 1] < 11) {
+    snakeObj.col.push(snakeObj.col[snakeObj.col.length - 1] + 1);
+  }
+  let shiftedElement = snakeObj.col.shift();
+  if (shiftedElement == 7) {
+    snakeObj.col.push(0);
+  }
+}
+
+function moveSnake(direction) {
+  if (direction == "horizontally") {
+    moveSnakeHorizontally();
+  }
+  if (direction == "vertically") {
+    moveSnakeObjectVertically();
+  }
+}
+
+function drawSnake() {
+  snakeObj.row.forEach((cell) => {
+    colorSnake(cell, 0);
+  });
+  snakeObj.col.forEach((col) => {
+    colorSnake(0, col);
+  });
+}
+
+function moveSnakeHorizontally() {
+  if (snake[snake.length - 1] < 9) {
+    snake.push(snake[snake.length - 1] + 1);
+  }
+  let shiftedElement = snake.shift();
+  if (shiftedElement == 9) {
+    snake.push(0);
+  }
+  drawSnakeHorizontally(snake, 0);
+}
+
+function moveSnakeVertically() {
+  if (snake[snake.length - 1] < 11) {
+    snake.push(snake[snake.length - 1] + 1);
+  }
+  let shiftedElement = snake.shift();
+  console.log(snake);
+  if (shiftedElement == 9) {
+    snake.push(0);
+  }
+  drawSnakeVertically(snake, 0);
+}
+
+let children = gameContainer.children;
+
+function drawSnakeHorizontally(snake, row) {
+  snake.forEach((element) => {
+    colorSnake(element, row);
+  });
+}
+
+function drawSnakeVertically(snake, column) {
+  snake.forEach((element) => {
+    colorSnake(column, element);
+  });
+}
+
+function colorSnake(x, y) {
+  let element = document.getElementById(`x${x}y${y}`);
+  element.classList.add("snake");
+}
+
+function resetField() {
+  for (let i = 0; i < gameContainer.children.length; i++) {
+    gameContainer.children[i].classList.remove("snake");
+  }
+}
+function removeSnake(x, y) {
+  gameContainer.children[i].classList.remove("snake");
+}
